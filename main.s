5d4964ba07a108864c0e1e80d773b9057991736e
cc1719 <cc1719@skbl-418-003.ph.ic.ac.uk>
2024-03-05 10:03:59 +0000

    Test 1

--- main.s
+++ main.s
@@ -8,15 +8,17 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
+	movlw	0xff
+	movwf	TRISD, A
 	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
+	movwf	TRISC, A	    ; Port C all outputs
 	bra 	test
 loop:
-	movff 	0x06, PORTB
 	incf 	0x06, W, A
+	movff 	0x06, PORTC
 test:
 	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
+	movf	PORTD, W, A
 	cpfsgt 	0x06, A
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
