9eb00dcf907983bc132ba6693bab58754862c75e
cc1719 <cc1719@skbl-418-003.ph.ic.ac.uk>
2024-03-05 14:54:12 +0000

    Unknown timer interrupts??

--- TimerInterrupt.s
+++ TimerInterrupt.s
@@ -1,35 +1,24 @@
 #include <xc.inc>
 	
-global	Timer1, timeH, timeL
-    
-psect	udata_acs   ; reserve data space in access ram
-timeH: ds   1	    ; reserve 1 byte for variable UART_counter
-timeL: ds   1    
+global	Timer0  
     
 psect	dac_code, class=CODE
 
-
-Timer1:
+Timer0:
+    btfsc	TMR2IF
+    goto	Timer1
     btfss	TMR0IF		; check that this is timer0 interrupt
     retfie	f		; if not then return
-    movlw	0xff
-    movwf	PORTD
-    movlw	0x0f
-    movwf	timeH
-    movlw	0x7D
-    movwf	timeL
-    call	bigdelay
-    bcf		TMR0IF	
-    movlw	0xff
-    movwf	TMR0L
-    movlw	0x6A
-    movwf	TMR0H
+    setf	PORTD
+    bsf		TMR2ON
+    bcf		TMR0IF
+    clrf	TMR0L
+    clrf	TMR0H
     retfie	f
 
-bigdelay:
-    movlw   0x0
-dloop:
-    decf    timeL, F, A
-    subwfb  timeH, F, A
-    bc	    dloop
-    return
+Timer1:
+    clrf    PORTD
+    bcf	    TMR2ON
+    bcf	    TMR2IF
+    clrf    TMR2
+    retfie  f
--- main.s
+++ main.s
@@ -1,6 +1,6 @@
 #include <xc.inc>
 
-extrn	Timer1, timeH, timeL
+extrn	Timer0
     
 psect	code, abs
 	
@@ -9,14 +9,20 @@ rst:	org	0x0
 
 int_hi:	
 	org	0x0008	; high vector, no low vector
-	goto	Timer1
+	goto	Timer0
 	
 setup:	clrf	TRISD
+	clrf	TMR0L
+	clrf	TMR0H
+	clrf	TMR1L
+	clrf	TMR1H
 	movlw	10000010B
-	movwf	T0CON, A
+	movwf	T0CON, A	; TMR0 is 20ms, time LOW
 	bsf	TMR0IE		; Enable timer0 interrupt
+	movlw	00100100B
+	movwf	T1CON, A	; TMR1 is <Duty Cycle>, time HIGH
+	bsf	TMR1IE
 	bsf	GIE
     
 main:
-	clrf	PORTD
-	goto	main
+	goto	$
