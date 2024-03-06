0b8b1b018e53978ad09de6a7ea2f306ce09b048f
ncwil <ncwil@192.168.1.168>
2024-03-06 13:04:52 +0000

    still working on keypad file

--- keypad.s
+++ keypad.s
@@ -1,60 +1,69 @@
 #include <xc.inc>
     
 global		KeyPad_Rows, KeyPad_Columns, KeyPad_Setup, Check_KeyPress
-
 psect		udata_acs   
 KeyPad_counter: ds  1       
 KeyPad_Value:   ds  1
-value:		ds  1  
    
 psect		KeyPad_code, class = CODE
 
-KeyPad_Setup:	clrf     LATE, A
-                movlb    0x0f
-                bsf      REPU
-                clrf     TRISD, A
+KeyPad_Setup:	clrf	LATE, A
+                movlb	0x0f
+                bsf     REPU
+                clrf    TRISD, A
                 return
+psect	code, abs
+rst:	org	0x0
+Table_Set_Up:   bcf     CFGS
+		bsf	EEPGD
+		db      0x11, 0x21, 0x41, 0x81, 0x12, 0x22, 0x42, 0x82
+		db	0x14, 0x24, 0x44, 0x84, 0x18, 0x28, 0x48, 0x88
+		Lookup_Table  EQU 0x300
+		counter	      EQU 0x10
+		align	      2
+
+Read_Lookup_Table:
+		lfsr    0, Lookup_Table
+		movlw   low highword(Table_Set_Up)
+		movwf   TBLPTRU, A
+		movlw   high(Table_Set_Up)
+		movwf   TBLPTRH, A
+		movlw   low(Table_Set_Up)
+		movwf   TBLPTRL, A
+		movlw   16   ; currently writes random values at start of table, don't know why
+		movwf   counter, A
+loop:		tblrd*+
+		movff   TABLAT, POSTINC0
+		decfsz  counter, A
+		bra     loop
+		goto    0
+
     
-KeyPad_Rows:	movlw    0x0f
-                movwf    TRISE, A
+KeyPad_Rows:	movlw   0x0f
+                movwf   TRISE, A
                 return
     
 KeyPad_Columns:
-                movlw    0xf0
-                movwf    TRISE, A
+                movlw   0xf0
+                movwf   TRISE, A
                 return
     
 Check_KeyPress:
-                call     KeyPad_Rows
-                call     delay
-                movff    PORTE, KeyPad_Value, A
-                call     KeyPad_Columns
-                call     delay
-                movlw    0x0f
-                andwf    KeyPad_Value, W, A
-                iorwf    PORTE, W, A
-                xorlw    0xff
-                movwf    KeyPad_Value, A
+                call    KeyPad_Rows
+                call    delay
+                movff   PORTE, KeyPad_Value, A
+                call    KeyPad_Columns
+                call    delay
+                movlw   0x0f
+                andwf   KeyPad_Value, W, A
+                iorwf   PORTE, W, A
+                xorlw   0xff
+                movwf   KeyPad_Value, A
                 return
-		
-Table_Set_Up:	FSR	 EQU 04
-		INDF	 EQU 00
-		movlw    00000001B
-		movwf	 value, A
-		movlw	 0x20
-		movwf    FSR
-loop:		movf     value, A
-	        movwf    INDF
-		incf     FSR
-		incf     value, A
-		movlw    00001010B
-		CPFSLT   value, A
-		return
-		goto     loop
                 
-delay:		movlw    0x40
-                movwf    KeyPad_counter, A
+delay:		movlw   0x40
+                movwf   KeyPad_counter, A
 		
-countdown:      decfsz   KeyPad_counter, A           
-                bra      countdown
+countdown:      decfsz  KeyPad_counter, A           
+                bra     countdown
                 return
