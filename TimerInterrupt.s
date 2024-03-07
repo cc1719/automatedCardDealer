3287b7af228760e35a94e74e9cbbb2d72622cfd8
cc1719 <cc1719@skbl-418-003.ph.ic.ac.uk>
2024-03-07 11:58:28 +0000

    Fully programmable PWM

--- TimerInterrupt.s
+++ TimerInterrupt.s
@@ -9,25 +9,25 @@ counterL: ds  1
 psect	dac_code, class=CODE
 
 PWM_Setup:
-	clrf	TRISD
-	movlw	0xc0		; 0xc0 for 20ms TMR0
-	movwf	TMR0L		; CHANGE BACK TO TMR0L
-	movlw	0x63		; 0x63 for 20ms TMR0
-	movwf	TMR0H		; CHANGE BACK TO TMR0H
-	movlw	10000010B
-	movwf	T0CON, A	; TMR0 is 20ms, time LOW
-	bsf	TMR0IE		; Enable timer0 interrupt
-	movlw	0x30
-	movwf	TMR3L
-	movlw	0xf8
-	movwf	TMR3H
-	movlw	00110000B
-	movwf	T3CON, A	; TMR3 is <Duty Cycle>, time HIGH
-	bsf	TMR3IP
-	bsf	TMR3IE
-	bsf	PEIE
-	bsf	GIE
-	return
+    clrf	TRISD
+    movlw	0xc0		; 0xc0 for 20ms TMR0
+    movwf	TMR0L		; CHANGE BACK TO TMR0L
+    movlw	0x63		; 0x63 for 20ms TMR0
+    movwf	TMR0H		; CHANGE BACK TO TMR0H
+    movlw	10000010B
+    movwf	T0CON, A	; TMR0 is 20ms, time LOW
+    bsf		TMR0IE		; Enable timer0 interrupt
+    movlw	0x30
+    movwf	TMR3L
+    movlw	0xf8
+    movwf	TMR3H
+    movlw	00100000B
+    movwf	T3CON, A	; TMR3 is <Duty Cycle>, time HIGH
+    bsf		TMR3IP
+    bsf		TMR3IE
+    bsf		PEIE
+    bsf		GIE
+    return
     
 Timer0:
     btfsc	TMR3IF
@@ -44,12 +44,12 @@ Timer0:
     retfie	f
 
 Timer3:
-    clrf    PORTD
-    bcf	    TMR3ON
-    bcf	    TMR3IF
-    movlw   0xd0
-    movwf   TMR3L
-    movlw   0x8a
-    movwf   TMR3H
-    retfie  f
+    clrf	PORTD
+    bcf		TMR3ON
+    bcf		TMR3IF
+    movlw	0x30
+    movwf	TMR3L
+    movlw	0xf8
+    movwf	TMR3H
+    retfie	f
 	
