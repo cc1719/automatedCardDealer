64778fe86ca10a6fb269901f0c6fd966ead6f83d
ncw21 <ncw21@skbl-418-003.ph.ic.ac.uk>
2024-03-12 15:46:55 +0000

    ...

--- main.s
+++ main.s
@@ -1,6 +1,6 @@
 #include <xc.inc>
  
-extrn   KeyPad_Setup, Check_KeyPress, KeyPad_Value, KeyPad_Output, LCD_Setup, LCD_Write_Message, LCD_clear, settingsInput
+extrn   KeyPad_Setup, Check_KeyPress, KeyPad_Value, KeyPad_Output, LCD_Setup, LCD_Write_Message, LCD_clear
     
 psect	udata_acs  
 variable: ds  1
@@ -10,9 +10,9 @@ psect	code, abs
 rst:	org	0x0
 	goto	setup
 	
-setup:	call    KeyPad_Setup
-	call    LCD_Setup
-	call    settingsInput
+setup:	;call    KeyPad_Setup
+	;call    LCD_Setup
+	
 	
 	
 	end	rst
