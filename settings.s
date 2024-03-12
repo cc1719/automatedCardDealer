890dc34bfd3cfb8a4c17742d9a35bf19bca05641
ncw21 <ncw21@skbl-418-003.ph.ic.ac.uk>
2024-03-12 15:46:51 +0000

    ...

--- settings.s
+++ settings.s
@@ -1,35 +1,64 @@
 #include <xc.inc>
 
-global  settingsInput
+extrn   LCD_Write_Message, LCD_line2
     
 psect	udata_acs  
-message:	ds  1
 counter:        ds  1
  
 psect		settings_code, class = CODE
 
-storedMessages: ;bcf     CFGS
+storedMessage1: ;bcf     CFGS
 		;bsf	EEPGD
 		db      'Enter no players'
-		db      'Enter no cards'
-		messages EQU 0x20
+		message1 EQU 0x20
 		align	 2
 
-settingsInput:  
-    
-outputPrompt1:	lfsr    2, messages
-		movlw   low highword(storedMessages)
+storedMessage2: db      'Enter no cards'
+		message2 EQU 0x40
+		align	 2
+
+readPrompt1:	lfsr    2, message1
+		movlw   low highword(storedMessage1)
 		movwf   TBLPTRU, A
-		movlw   high(storedMessages)
+		movlw   high(storedMessage1)
 		movwf   TBLPTRH, A
-		movlw   low(storedMessages)
+		movlw   low(storedMessage1)
 		movwf   TBLPTRL, A
 		movlw   20
 		movwf   counter, A
-loop:		tblrd*+
+loop1:		tblrd*+
 		movff   TABLAT, POSTINC2
 		decfsz  counter, A
-		bra     loop
+		bra     loop1
 		return
 
+readPrompt2:	lfsr    2, message2
+		movlw   low highword(storedMessage2)
+		movwf   TBLPTRU, A
+		movlw   high(storedMessage2)
+		movwf   TBLPTRH, A
+		movlw   low(storedMessage2)
+		movwf   TBLPTRL, A
+		movlw   14
+		movwf   counter, A
+loop2:		tblrd*+
+		movff   TABLAT, POSTINC2
+		decfsz  counter, A
+		bra     loop2
+		return
+		
+int_hi:         org     0x0008
+		goto    timer4
 
+timer4:		btfss   TMR4IF
+		retfie  f
+		
+		
+		
+settingsInput:  call    readPrompt1
+		movf    counter, 0
+		call    LCD_Write_Message
+		call    LCD_line2
+		goto    $
+continue1:	
+		
