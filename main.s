1606b356877502de18449c1b156ec770dad1b3ef
ncw21 <ncw21@skbl-418-003.ph.ic.ac.uk>
2024-03-12 15:07:58 +0000

    ...

--- main.s
+++ main.s
@@ -1,19 +1,16 @@
 #include <xc.inc>
+ 
+extrn   KeyPad_Setup, Check_KeyPress, KeyPad_Value, KeyPad_Output, LCD_Setup, LCD_Write_Message, LCD_clear
     
 psect	udata_acs  
 variable: ds  1
 
 psect	code, abs
-	
+
 rst:	org	0x0
 	goto	setup
 	
-setup:	
-	clrf    TRISD
-	movlw   0xff
-	comf    PORTD, F
-	clrf    TRISE
-	movlw   0xff
-	comf    PORTE, F
-	goto	setup
+setup:	;call    KeyPad_Setup
+	;call    LCD_Setup
+	
 	end	rst
