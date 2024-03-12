5b14c9151e4df98c5816b40f3b00d8d0301982f9
ncwil <ncwil@192.168.1.168>
2024-03-12 20:56:36 +0000

    ..

--- main.s
+++ main.s
@@ -1,6 +1,6 @@
 #include <xc.inc>
  
-extrn   KeyPad_Setup, Check_KeyPress, KeyPad_Value, KeyPad_Output, LCD_Setup, LCD_Write_Message, LCD_clear
+extrn   KeyPad_Setup, Check_KeyPress, KeyPad_Value, KeyPad_Output, LCD_Setup, LCD_Write_Message, LCD_clear, settingsInput
     
 psect	udata_acs  
 variable: ds  1
@@ -8,10 +8,10 @@ variable: ds  1
 psect	code, abs
 
 rst:	org	0x0
-	goto	setup
+	goto	main
 	
-setup:	;call    KeyPad_Setup
-	;call    LCD_Setup
+main:   call settingsInput
+
 	
 	
 	
