ad96e9fa184a0fc2f962ec274233d205f9428854
ncw21 <ncw21@skbl-418-003.ph.ic.ac.uk>
2024-03-12 09:45:29 +0000

    can write single characters, need to implement interrupt

--- main.s
+++ main.s
@@ -8,8 +8,6 @@ psect	udata_acs   ; reserve data space in access ram
     dutytimeL:      ds  1
     dutytimeH:      ds  1
     delayCounter:   ds  1
-    counter:        ds  1
-    address:	    ds  1
     
 psect	data
 	dutycycle EQU 3784
@@ -19,9 +17,9 @@ psect	code, abs
 rst:	org	0x0
 	goto	setup
 
-int_hi:	
-	org	0x0008	; high vector, no low vector
-	goto	Timer0
+;int_hi:	
+;	org	0x0008	; high vector, no low vector
+;	goto	Timer0
 	
 setup:	
 ;	movlw	HIGH(dutycycle)
@@ -34,33 +32,19 @@ setup:
 ;	call	PWM_Setup
 ;	call	ADC_Setup
 	call    KeyPad_Setup
-type:	movlw   0
-	movwf   KeyPad_Value, 0
-	lfsr    2, 0x20
-	movlw   0
-	movwf   counter
-	movlw   1
-	addwf   counter
-	movlw   0x1F
-	movwf   address
-	movlw   1
-	addwf   address
-loop:	call    Check_KeyPress
+loop:	movlw   0
+	movwf   KeyPad_Value
+	call    Check_KeyPress
 	tstfsz  KeyPad_Value, 0
 	goto    next
 	goto    loop
 next:	call    KeyPad_Output
-	movff   KeyPad_Value, address
-	movf    counter, 0
-	call    LCD_Write_Message
+	movf    KeyPad_Value, 0, 0
+	call    LCD_Send_Byte_D
 	call    delay
-main:	movlw   0
-	movwf   KeyPad_Value
-	goto	type
-	
-delay:	movlw   0x40
+	goto    loop
+delay:	movlw   0x2
         movwf   delayCounter, A
-		
 count:  decfsz  delayCounter, A           
         bra     delayCounter
         return
