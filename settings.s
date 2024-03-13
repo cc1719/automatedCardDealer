b6b5fbce8e3fc5ee68118b8737efd6d6aae496df
ncw21 <ncw21@skbl-418-003.ph.ic.ac.uk>
2024-03-13 12:54:19 +0000

    ...

--- settings.s
+++ settings.s
@@ -1,32 +1,62 @@
 #include <xc.inc>
- 
-;psect	udata_acs  
-;message:	ds  1
-;counter:        ds  1
+
+extrn   LCD_Write_Message, LCD_line2, LCD_Setup, KeyPad_Setup, Check_KeyPress, KeyPad_Value, KeyPad_Output, writeNumPlayers, writeNumCards, numPlayersDigit1, numPlayersDigit2, numCardsDigit1, numCardsDigit2
+global  settingsInput
+psect	udata_acs  
+counter:        ds  1
  
 psect		settings_code, class = CODE
 
-storedMessages: ;bcf     CFGS
-		;bsf	EEPGD
-		;db      'Enter no players'
-		;db      'Enter no cards'
-		;messages EQU 0x20
-		;align	 2
+storedMessage1: bcf     CFGS
+		bsf	EEPGD
+		db      'Enter no players'
+		message1 EQU 0x20
+		align	 2
 
-readMessage:    ;lfsr    0, messages
-		;movlw   low highword(storedMessages)
-		;movwf   TBLPTRU, A
-		;movlw   high(storedMessages)
-		;movwf   TBLPTRH, A
-		;movlw   low(storedMessages)
-		;movwf   TBLPTRL, A
-		;movlw   10
-		;movwf   counter, A
-loop:		;tblrd*+
-		;movff   TABLAT, POSTINC0
-		;decfsz  counter, A
-		;bra     loop
+storedMessage2: db      'Enter no cards'
+		message2 EQU 0x40
+		align	 2
+
+readPrompt1:	lfsr    2, message1
+		movlw   low highword(storedMessage1)
+		movwf   TBLPTRU, A
+		movlw   high(storedMessage1)
+		movwf   TBLPTRH, A
+		movlw   low(storedMessage1)
+		movwf   TBLPTRL, A
+		movlw   20
+		movwf   counter, A
+loop1:		tblrd*+
+		movff   TABLAT, POSTINC2
+		decfsz  counter, A
+		bra     loop1
 		return
 
+readPrompt2:	lfsr    2, message2
+		movlw   low highword(storedMessage2)
+		movwf   TBLPTRU, A
+		movlw   high(storedMessage2)
+		movwf   TBLPTRH, A
+		movlw   low(storedMessage2)
+		movwf   TBLPTRL, A
+		movlw   14
+		movwf   counter, A
+loop2:		tblrd*+
+		movff   TABLAT, POSTINC2
+		decfsz  counter, A
+		bra     loop2
+		return    
 
+settingsInput:  call    readPrompt1
+		movf    counter, 0, 0
+		call    LCD_Write_Message
+		call    LCD_line2
+		call    writeNumPlayers	
+		
+		call    readPrompt2
+		movf    counter, 0, 0
+		call    LCD_Write_Message
+		call    LCD_line2
+		call    writeNumCards	
+		return
 
