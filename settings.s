4f8177df65903a114591c453975b52f9a83f7988
ncw21 <ncw21@skbl-418-003.ph.ic.ac.uk>
2024-03-13 15:48:28 +0000

    ...

--- settings.s
+++ settings.s
@@ -1,23 +1,24 @@
 #include <xc.inc>
 
 extrn   LCD_clear, LCD_Write_Message, LCD_line2, LCD_Setup, KeyPad_Setup, Check_KeyPress, KeyPad_Value, KeyPad_Output, writeNumPlayers, writeNumCards, numPlayersDigit1, numPlayersDigit2, numCardsDigit1, numCardsDigit2
-global  settingsInput, msg1, msg2, message1, message2
+global  settingsInput, msg1, msg2, message1, message2, count1, count2
 psect	udata_acs  
 counter:        ds  1
 msg1:		ds  1
 msg2:		ds  1
-message1:	ds  1
-message2:	ds  1
 count1:		ds  1
 count2:		ds  1
  
 psect		settings_code, class = CODE
 
 storedMessage1: db     'Enter no players'
+		message1  EQU 0x200
 storedMessage2: db     'Enter no cards'
-readPrompt1:	movlw   0x20
+		message2  EQU 0x220
+readPrompt1:	bcf	CFGS
+		bsf	EEPGD 
+		movlw   0x200
 		movwf   msg1, A	
-		movwf   message1, A	
 		lfsr    2, message1
 		movlw   low highword(storedMessage1)
 		movwf   TBLPTRU, A
@@ -26,17 +27,19 @@ readPrompt1:	movlw   0x20
 		movlw   low(storedMessage1)
 		movwf   TBLPTRL, A
 		movlw   17
-		movwf   count1, 0
+		movwf   count1, A
+		movlw   17
 		movwf   counter, A
 loop1:		tblrd*+
 		movff   TABLAT, POSTINC2
 		decfsz  counter, A
-		bra     loop1
+		goto    loop1
 		return
 
-readPrompt2:	movlw   0x40
-		movwf   msg2, A
-		movwf   message2, A	
+readPrompt2:	bcf	CFGS
+		bsf	EEPGD 
+		movlw   0x220
+		movwf   msg2, A	
 		lfsr    2, message2
 		movlw   low highword(storedMessage2)
 		movwf   TBLPTRU, A
@@ -50,14 +53,14 @@ readPrompt2:	movlw   0x40
 loop2:		tblrd*+
 		movff   TABLAT, POSTINC2
 		decfsz  counter, A
-		bra     loop2
+		goto     loop2
 		return    
 
 settingsInput:	call    LCD_Setup
 		call    LCD_clear
 		call    readPrompt1
 		movf    count1, 0, 0
-		lfsr    2, msg1
+		lfsr    2, 0x200
 		call    LCD_Write_Message
 		call    LCD_line2
 		call    writeNumPlayers	
