b8cb89a71b86c8bb299775c31235607f1f64e7d3
ncw21 <ncw21@skbl-418-003.ph.ic.ac.uk>
2024-03-13 15:19:45 +0000

    ...

--- settings.s
+++ settings.s
@@ -1,20 +1,23 @@
 #include <xc.inc>
 
 extrn   LCD_clear, LCD_Write_Message, LCD_line2, LCD_Setup, KeyPad_Setup, Check_KeyPress, KeyPad_Value, KeyPad_Output, writeNumPlayers, writeNumCards, numPlayersDigit1, numPlayersDigit2, numCardsDigit1, numCardsDigit2
-global  settingsInput, msg1, msg2
+global  settingsInput, msg1, msg2, message1, message2
 psect	udata_acs  
 counter:        ds  1
 msg1:		ds  1
 msg2:		ds  1
+message1:	ds  1
+message2:	ds  1
+count1:		ds  1
+count2:		ds  1
  
 psect		settings_code, class = CODE
 
 storedMessage1: db     'Enter no players'
-		message1 EQU 0x20
 storedMessage2: db     'Enter no cards'
-		message2 EQU 0x40
 readPrompt1:	movlw   0x20
-		movwf   msg1, A		
+		movwf   msg1, A	
+		movwf   message1, A	
 		lfsr    2, message1
 		movlw   low highword(storedMessage1)
 		movwf   TBLPTRU, A
@@ -23,6 +26,7 @@ readPrompt1:	movlw   0x20
 		movlw   low(storedMessage1)
 		movwf   TBLPTRL, A
 		movlw   17
+		movwf   count1, 0
 		movwf   counter, A
 loop1:		tblrd*+
 		movff   TABLAT, POSTINC2
@@ -32,6 +36,7 @@ loop1:		tblrd*+
 
 readPrompt2:	movlw   0x40
 		movwf   msg2, A
+		movwf   message2, A	
 		lfsr    2, message2
 		movlw   low highword(storedMessage2)
 		movwf   TBLPTRU, A
@@ -40,6 +45,7 @@ readPrompt2:	movlw   0x40
 		movlw   low(storedMessage2)
 		movwf   TBLPTRL, A
 		movlw   15
+		movwf   count2, 0
 		movwf   counter, A
 loop2:		tblrd*+
 		movff   TABLAT, POSTINC2
@@ -47,16 +53,17 @@ loop2:		tblrd*+
 		bra     loop2
 		return    
 
-settingsInput:	call    LCD_clear
+settingsInput:	call    LCD_Setup
+		call    LCD_clear
 		call    readPrompt1
-		movf    counter, 0, 0
+		movf    count1, 0, 0
 		lfsr    2, msg1
 		call    LCD_Write_Message
 		call    LCD_line2
 		call    writeNumPlayers	
 		
 		call    readPrompt2
-		movf    counter, 0, 0
+		movf    count2, 0, 0
 		call    LCD_Write_Message
 		call    LCD_line2
 		call    writeNumCards	
