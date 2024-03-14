1667fa9cbd04b5f35ac9c3c8beda5c52e1168169
ncw21 <ncw21@skbl-418-003.ph.ic.ac.uk>
2024-03-14 11:45:51 +0000

    ...

--- main.s
+++ main.s
@@ -1,39 +1,13 @@
 #include <xc.inc>
 
-extrn   ADC_Setup, ADC_Read, LCD_clear, settingsInput
-
-global	dutytimeL, dutytimeH
-    
-psect	udata_acs   ; reserve data space in access ram
-    dutytimeL:      ds  1
-    dutytimeH:      ds  1
-    delayCounter:   ds  1
-    variable:	    ds  1
-    
-psect	data
-	dutycycle EQU 3784
+extrn   settingsInput
 
 psect	code, abs
 	
 rst:	org	0x0
 	goto	setup
-
-;int_hi:	
-;	org	0x0008	; high vector, no low vector
-;	goto	Timer0
 	
-setup:	
-;	movlw	HIGH(dutycycle)
-;	movwf	dutytimeH, A
-;	movlw	LOW(dutycycle)
-;	movwf	dutytimeL, A
-;	bcf	CFGS	; point to Flash program memory  
-;	bsf	EEPGD 	; access Flash program memory
-;	call	LCD_Setup	; setup UART
-;	call	PWM_Setup
-;	call	ADC_Setup
-;	call    KeyPad_Setup
-	call    settingsInput
+setup:	call    settingsInput
 	goto    $
 	
 	end rst
