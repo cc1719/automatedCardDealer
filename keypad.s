9084de4ed34edf929be13c34a773ed0339f924f8
ncw21 <ncw21@skbl-418-003.ph.ic.ac.uk>
2024-03-14 14:13:50 +0000

    ...

--- keypad.s
+++ keypad.s
@@ -21,7 +21,7 @@ delayVariable:	ds  1
    
 psect		KeyPad_code, class = CODE
 
-KeyPad_Setup:	clrf	LATC, A
+KeyPad_Setup:	clrf	LATJ, A
                 movlb	0x0f
                 bsf     REPU
                 clrf    TRISD, A
@@ -34,24 +34,24 @@ Table_Set_Up:   db      00110001B, 00110100B, 00110111B, 01000001B
 		Lookup_Table  EQU 0x300
 
 KeyPad_Rows:	movlw   0x0f
-                movwf   TRISC, A
+                movwf   TRISJ, A
                 return
     
 KeyPad_Columns:
                 movlw   0xf0
-                movwf   TRISC, A
+                movwf   TRISJ, A
                 return
     
 Check_KeyPress: movlw   0
 		movwf   KeyPad_Value, A
                 call    KeyPad_Rows
                 call    delay
-                movff   PORTC, KeyPad_Value, A
+                movff   PORTJ, KeyPad_Value, A
                 call    KeyPad_Columns
                 call    delay
                 movlw   0x0f
                 andwf   KeyPad_Value, W, A
-                iorwf   PORTC, W, A
+                iorwf   PORTJ, W, A
                 xorlw   0xff
                 movwf   KeyPad_Value, A
 
@@ -168,11 +168,11 @@ there1:		call    LCD_Send_Byte_D
 		movff   KeyPad_Value, numPlayersDigit1
 		movlw   1
 		movwf   test, A
-here1:		movf    PORTC, 0, 0
+here1:		movf    PORTJ, 0, 0
 		cpfseq  checkIfPressed, 0
 		goto    here1
 		goto    everywhere1	
-somewhere1:	movf    PORTC, 0, 0
+somewhere1:	movf    PORTJ, 0, 0
 		cpfseq  checkIfPressed, 0
 		goto    somewhere1
 		movff   KeyPad_Value, numPlayersDigit2
@@ -203,11 +203,11 @@ there2:		call    LCD_Send_Byte_D
 		movff   KeyPad_Value, numCardsDigit1
 		movlw   1
 		movwf   test, A
-here2:		movf    PORTC, 0, 0
+here2:		movf    PORTJ, 0, 0
 		cpfseq  checkIfPressed, 0
 		goto    here2
 		goto    everywhere2	
-somewhere2:	movf    PORTC, 0, 0
+somewhere2:	movf    PORTJ, 0, 0
 		cpfseq  checkIfPressed, 0
 		goto    somewhere2
 		movff   KeyPad_Value, numCardsDigit2
