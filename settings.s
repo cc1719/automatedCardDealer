bd05c089bbb3a231826dfe7e10f993229b058c53
ncw21 <ncw21@skbl-418-004.ph.ic.ac.uk>
2024-03-19 10:48:25 +0000

    ...

--- settings.s
+++ settings.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
 
 extrn   LCD_delay_ms, LCD_clear, LCD_Write_Message, LCD_line2, LCD_Setup, KeyPad_Setup, Check_KeyPress, KeyPad_Value, KeyPad_Output, writeNumPlayers, writeNumCards, numPlayersDigit1, numPlayersDigit2, numCardsDigit1, numCardsDigit2
-global  settingsSetup, settingsInput, count1, count2, numPlayers, numCards
+global  Settings_Setup, Settings_Input, count1, count2, numPlayers, numCards
 psect	udata_acs  
 counter:        ds  1
 count1:		ds  1
@@ -12,23 +12,23 @@ numCards:	ds  1
  
 psect		settings_code, class = CODE
 
-storedMessage1: db     'Enter no players'
+Stored_Message1: db     'Enter no players'
 		message1  EQU 0x240
-storedMessage2: db     'Enter no cards'
+Stored_Message2: db     'Enter no cards'
 		message2  EQU 0x250
-storedMessage3:	db     'Deal again?'
+Stored_Message3:	db     'Deal again?'
 		message3  EQU 0x260
   
-settingsSetup:	call    KeyPad_Setup
+Settings_Setup:	call    KeyPad_Setup
 		call    LCD_Setup
 		return
 		
-readPrompt1:	lfsr    2, message1
-		movlw   low highword(storedMessage1)
+Read_Prompt1:	lfsr    2, message1
+		movlw   low highword(Stored_Message1)
 		movwf   TBLPTRU, A
-		movlw   high(storedMessage1)
+		movlw   high(Stored_Message1)
 		movwf   TBLPTRH, A
-		movlw   low(storedMessage1)
+		movlw   low(Stored_Message1)
 		movwf   TBLPTRL, A
 		movlw   17
 		movwf   count1,  A
@@ -39,12 +39,12 @@ loop1:		tblrd*+
 		goto    loop1
 		return
 
-readPrompt2:	lfsr    2, message2
-		movlw   low highword(storedMessage2)
+Read_Prompt2:	lfsr    2, message2
+		movlw   low highword(Stored_Message2)
 		movwf   TBLPTRU, A
-		movlw   high(storedMessage2)
+		movlw   high(Stored_Message2)
 		movwf   TBLPTRH, A
-		movlw   low(storedMessage2)
+		movlw   low(Stored_Message2)
 		movwf   TBLPTRL, A
 		movlw   14
 		movwf   count2,  A
@@ -55,12 +55,12 @@ loop2:		tblrd*+
 		goto    loop2
 		return    
 
-readPrompt3:	lfsr    2, message3
-		movlw   low highword(storedMessage3)
+Read_Prompt3:	lfsr    2, message3
+		movlw   low highword(Stored_Message3)
 		movwf   TBLPTRU, A
-		movlw   high(storedMessage3)
+		movlw   high(Stored_Message3)
 		movwf   TBLPTRH, A
-		movlw   low(storedMessage3)
+		movlw   low(Stored_Message3)
 		movwf   TBLPTRL, A
 		movlw   11
 		movwf   count3,  A
@@ -71,8 +71,8 @@ loop3:		tblrd*+
 		goto    loop3
 		return    
 
-settingsInput:	call    LCD_clear
-		call    readPrompt1
+Settings_Input:	call    LCD_clear
+		call    Read_Prompt1
 		movf    count1, 0, 0
 		lfsr    2, message1
 		call    LCD_Write_Message
@@ -81,7 +81,7 @@ settingsInput:	call    LCD_clear
 		movlw   100
 		call    LCD_delay_ms
 		call    LCD_clear
-		call    readPrompt2
+		call    Read_Prompt2
 		movf    count2, 0, 0
 		lfsr    2, message2
 		call    LCD_Write_Message
@@ -93,12 +93,12 @@ settingsInput:	call    LCD_clear
 		
 		movlw   0xff
 		cpfseq  numPlayersDigit2, 0
-		goto    twoDigitPlayers	
+		goto    Two_Digit_Players	
 		movlw   48
 		subwf   numPlayersDigit1, 0, 0
 		movwf   numPlayers, A
 		goto    skip
-twoDigitPlayers:movlw   48
+Two_Digit_Players:movlw   48
 		subwf   numPlayersDigit1, 1, 0
 		movlw   10
 		mulwf   numPlayersDigit1, 0
@@ -109,13 +109,13 @@ twoDigitPlayers:movlw   48
 		
 skip:		movlw   0xff
 		cpfseq  numCardsDigit2, 0
-		goto    twoDigitCards	
+		goto    Two_Digit_Cards	
 		movlw   48
 		subwf   numCardsDigit1, 0, 0
 		movwf   numCards, A
 		return
 		
-twoDigitCards:	movlw   48
+Two_Digit_Cards:	movlw   48
 		subwf   numCardsDigit1, 1, 0
 		movlw   10
 		mulwf   numCardsDigit1, 0
