8dfa79ecec6938f009a8c4f3197567d0a63525c4
ncw21 <ncw21@skbl-418-004.ph.ic.ac.uk>
2024-03-19 10:41:12 +0000

    ...

--- settings.s
+++ settings.s
@@ -1,7 +1,7 @@
 #include <xc.inc>
 
 extrn   LCD_delay_ms, LCD_clear, LCD_Write_Message, LCD_line2, LCD_Setup, KeyPad_Setup, Check_KeyPress, KeyPad_Value, KeyPad_Output, writeNumPlayers, writeNumCards, numPlayersDigit1, numPlayersDigit2, numCardsDigit1, numCardsDigit2
-global  settingsInput, count1, count2, numPlayers, numCards
+global  settingsSetup, settingsInput, count1, count2, numPlayers, numCards
 psect	udata_acs  
 counter:        ds  1
 count1:		ds  1
