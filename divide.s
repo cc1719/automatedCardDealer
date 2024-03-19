4c7d72980706c20e699d33ffb98346c40779b845
ncwil <ncwil@192.168.1.168>
2024-03-19 21:05:34 +0000

    ..

--- divide.s
+++ divide.s
@@ -6,8 +6,7 @@ extrn numPlayers
     
 psect	udata_acs
 arg1:	 ds 1 
-arg2:	 ds 1 ; arg1 + arg2 should equal one factor of the total number to be divided - max = 510, dont divide by 1.
-arg3:	 ds 1 ; arg3 should equal the other factor (max 8 bit).
+arg2:	 ds 1 
 output:   ds 1
 remainder1: ds 1
 remainder2: ds 1
@@ -27,27 +26,27 @@ divide:    ; divides (arg1+arg2) by numPlayers, result in output.
 		movwf   output, 0
     	        movf    arg1, 0, 0
 		cpfsgt  divisor, 0
-		goto    continue3
-		goto    end3
-continue3:	movf    divisor, 0, 0
+		goto    next1
+		goto    end1
+next1:		movf    divisor, 0, 0
 		subwf   arg1, 1, 0
 		incf    output, 1, 0
 		movf    arg1, 0, 0
 		cpfsgt  divisor, 0
-		goto    continue3
-end3:		movwf   remainder1, 0	
+		goto    next1
+end1:		movwf   remainder1, 0	
     
 		movf    arg2, 0, 0
 		cpfsgt  divisor, 0
-		goto    continue4
-		goto    end4
-continue4:	movf    divisor, 0, 0
+		goto    next2
+		goto    end2
+next2:		movf    divisor, 0, 0
 		subwf   arg2, 1, 0
 		incf    output, 1, 0
 		movf    arg2, 0, 0
 		cpfsgt  divisor, 0
-		goto    continue4
-end4:		movwf   remainder2, 0
+		goto    next2
+end2:		movwf   remainder2, 0
 		
 		movf    remainder1, 0, 0
 		addwf   remainder2, 0, 0
