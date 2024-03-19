62e33d466adce30f9de6d2df988eb2788ba30daf
ncwil <ncwil@192.168.1.168>
2024-03-19 20:56:55 +0000

    ..

--- settings.s
+++ settings.s
@@ -4,85 +4,62 @@ extrn   resetVar, Write_Y_Or_N, LCD_delay_ms, LCD_clear, LCD_Write_Message, LCD_
 global  Settings_Setup, Settings_Input, count1, count2, numPlayers, numCards, Reset_Settings
 psect	udata_acs  
 counter:        ds  1
-count1:		ds  1
-count2:		ds  1
-count3:		ds  1
+count:		ds  1
+messageLocation:	ds  1
+message:	ds  1
 numCards:	ds  1
  
 psect		settings_code, class = CODE
 
 Stored_Message1: db     'Enter no players'
-		message1  EQU 0x240
+		messageLocation1  EQU 0x240
 Stored_Message2: db     'Enter no cards'
-		message2  EQU 0x250
+		messageLocation2  EQU 0x250
 Stored_Message3: db     'Re-deal? 1-Y 2-N'
-		message3  EQU 0x260
+		messageLocation3  EQU 0x260
+Stored_Message4: db     'Dealing...'
+		messageLocation4  EQU 0x270
   
 Settings_Setup:	call    KeyPad_Setup
 		call    LCD_Setup
 		return
 		
-Read_Prompt1:	lfsr    2, message1
-		movlw   low highword(Stored_Message1)
+Read_Prompt:	lfsr    2, messageLocation
+		movlw   low highword(message)
 		movwf   TBLPTRU, A
-		movlw   high(Stored_Message1)
+		movlw   high(message)
 		movwf   TBLPTRH, A
-		movlw   low(Stored_Message1)
+		movlw   low(message)
 		movwf   TBLPTRL, A
-		movlw   17
-		movwf   count1,  A
-		movwf   counter, A
-loop1:		tblrd*+
+loop:		tblrd*+
 		movff   TABLAT, POSTINC2
 		decfsz  counter, A
-		goto    loop1
+		goto    loop
 		return
 
-Read_Prompt2:	lfsr    2, message2
-		movlw   low highword(Stored_Message2)
-		movwf   TBLPTRU, A
-		movlw   high(Stored_Message2)
-		movwf   TBLPTRH, A
-		movlw   low(Stored_Message2)
-		movwf   TBLPTRL, A
-		movlw   14
-		movwf   count2,  A
-		movwf   counter, A
-loop2:		tblrd*+
-		movff   TABLAT, POSTINC2
-		decfsz  counter, A
-		goto    loop2
-		return    
-
-Read_Prompt3:	lfsr    2, message3
-		movlw   low highword(Stored_Message3)
-		movwf   TBLPTRU, A
-		movlw   high(Stored_Message3)
-		movwf   TBLPTRH, A
-		movlw   low(Stored_Message3)
-		movwf   TBLPTRL, A
-		movlw   11
-		movwf   count3,  A
-		movwf   counter, A
-loop3:		tblrd*+
-		movff   TABLAT, POSTINC2
-		decfsz  counter, A
-		goto    loop3
-		return    
-
 Settings_Input:	call    LCD_clear
-		call    Read_Prompt1
-		movf    count1, 0, 0
-		lfsr    2, message1
+		movlw   17
+		movwf   count, 0
+		movwf   counter, 0
+		movff   messageLocation1, messageLocation
+		movff   Stored_Message1, message
+		call    Read_Prompt
+		movf    count, 0, 0
+		lfsr    2, messageLocation1
 		call    LCD_Write_Message
 		call    LCD_line2
 		call    writeNumPlayers	
 		movlw   100
 		call    LCD_delay_ms
 		call    LCD_clear
-		call    Read_Prompt2
-		movf    count2, 0, 0
-		lfsr    2, message2
+		movlw   14
+		movwf   count, 0
+		movwf   counter, 0
+		movff   messageLocation2, messageLocation
+		movff   Stored_Message2, message
+		call    Read_Prompt
+		movf    count, 0, 0
+		lfsr    2, messageLocation2
 		call    LCD_Write_Message
 		call    LCD_line2
 		call    writeNumCards	
@@ -112,9 +89,14 @@ Two_Digit_Cards:	movlw   48
 		return
 
 Reset_Settings:	call    LCD_clear
-		call    Read_Prompt3
-		movf    count3, 0, 0
-		lfsr    2, message3
+		movlw   11
+		movwf   count, 0
+		movwf   counter, 0
+		movff   messageLocation3, messageLocation
+		movff   Stored_Message3, message
+		call    Read_Prompt
+		movf    count, 0, 0
+		lfsr    2, messageLocation3
 		call    LCD_Write_Message
 		call    LCD_line2
 		call    Write_Y_Or_N
